508accb3789028277ddd869bc154d473eb539f96
danielargolo-git <d.argolo3d@gmail.com>
2022-11-24 23:42:34 -0300

    Simple data and time exercise

---
+++ JS/Exercises/datatime/datatime.js
@@ -0,0 +1,14 @@
+let today = new Date();
+
+let day = today.getDay();
+let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("Today is : " + daylist[day] + ".");
+
+
+let hour = today.getHours;
+let minutes = today.getMinutes;
+let seconds = today.getSeconds;
+
+console.log("Current time is : " + hour + ":" + minutes + ":" + seconds);
+
