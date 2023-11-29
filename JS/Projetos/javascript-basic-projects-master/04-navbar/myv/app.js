f2f4728e7f112e6831318d56bc24d619c72e6caa
Daniel Argôlo <dimensionalrings@gmail.com>
2023-11-29 16:36:01 -0300

    js practice

---
+++ JS/Projetos/javascript-basic-projects-master/04-navbar/myv/app.js
@@ -0,0 +1,12 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+
+//Um navbar que quando a dimensão da janela chegue a X, será transformado em um ícone que quando clicado abrirá um menu nav abaixo do título
+//vou precisar do nav e dos componentes do nav
+//Os componentes do nav precisam estar dentro do mesmo nav
+
+
